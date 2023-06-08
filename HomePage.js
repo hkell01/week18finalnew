1838bfc833f6fd35b6eb3d3e3d812ad04aba2f47
Hayden Kelley <haydenkelley11@gmail.com>
2023-06-08 11:49:14 -0500

    Add files via upload

---
+++ HomePage.js
@@ -0,0 +1,13 @@
+// HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      {/* Add your home page content here */}
+    </div>
+  );
+};
+
+export default HomePage;
